d2bcd0f93ae8ec4008b1cb4f8085b435950be852
Alexandra Voit <alexandra.voit@ut.ee>
2025-09-23 23:51:51 +0300

    Initial module UI indication -> placeholder colors, incomplete logic

--- src/components/planner/SemesterGrid/CourseBox.js
+++ src/components/planner/SemesterGrid/CourseBox.js
@@ -1,11 +1,12 @@
 import { Text } from '@primer/react';
 import {useCourse} from '../../../context';
-import styles from './SemesterGrid.module.css'
+import styles from './CourseBox.module.css'
 
 function CourseBox({ course, onDragStart, draggable }) {
 
     const { getCourseIssues, openPane } = useCourse();
     const issues = getCourseIssues(course.id);
+    const moduleClass = course.module ? course.module.toLowerCase() : 'none';
 
     const handleDragStart = (event) => {
     if (onDragStart && course) {
@@ -15,7 +16,7 @@ function CourseBox({ course, onDragStart, draggable }) {
 
   return (
     <div
-        className={`${styles.courseBox} ${!issues.ok ? styles.hasIssues : ''}`}
+        className={`${styles.courseBox} ${styles[moduleClass]} ${!issues.ok ? styles.issues : ''}`}
         draggable={draggable}
         onDragStart={draggable ? handleDragStart : undefined}
         onClick={() => openPane(course)} //
