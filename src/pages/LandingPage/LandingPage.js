d0c8155630eb48111237f3cd6d7786488b207171
Alexandra Voit <145194484+alexandravoit@users.noreply.github.com>
2026-02-25 19:29:40 +0200

    Yassify the landing page

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,22 +1,60 @@
-import {Blankslate} from '@primer/react/experimental'
-import {DatabaseIcon} from '@primer/octicons-react'
+import {Hero, Card, ThemeProvider as BrandTheme} from '@primer/react-brand';
+import { ReactComponent as BigLogo } from '../../assets/BACHLOG-OUTLINE-LOGO.svg'
 import styles from './LandingPage.module.css'
+import React from "react";
 
 function LandingPage() {
 
     return (
-      <div className={styles.landingPage}>
-        <Blankslate border narrow>
-          <Blankslate.Visual>
-            <DatabaseIcon size="medium" />
-          </Blankslate.Visual>
-          <Blankslate.Heading>BACHLOG -v 2.0.0</Blankslate.Heading>
-          <Blankslate.Description>Su baka backlog.</Blankslate.Description>
-          <Blankslate.PrimaryAction as="a" href="/planner">
-            Mine planeerima
-          </Blankslate.PrimaryAction>
-        </Blankslate>
-      </div>
+        <BrandTheme colorMode="auto">
+            <div className={styles.landingPage}>
+
+                <div className={styles.hero}>
+                    <Hero>
+                        <Hero.Heading>BACHLOG:</Hero.Heading>
+                        <Hero.Heading>Sinu baka <em>backlog</em></Hero.Heading>
+                        <Hero.Description>
+                            Bakalaureuse õppeplaani koostamiseks ja valideerimiseks mõeldud rakendus Tartu Ülikooli tudengitele.
+                        </Hero.Description>
+                    </Hero>
+
+                    <BigLogo className={styles.logo} />
+
+                </div>
+
+
+                <div className={styles.cards}>
+                    <div>
+                        <Card ctaText="" href="/planner" hasBorder fullWidth={true} >
+                            <Card.Heading>Kursused</Card.Heading>
+                            <Card.Description>
+                                Otsi kursuseid. Saa infot nende kuuluvuse, eeldusainete ning toimumisaja kohta.
+                            </Card.Description>
+                        </Card>
+                    </div>
+
+                    <div>
+                        <Card ctaText="" href="/planner" hasBorder fullWidth={true} >
+                            <Card.Heading>Semestrid</Card.Heading>
+                            <Card.Description>
+                                Planeeri kursuseid semestrite kaupa. Kontrolli plaani korrektsust.
+                            </Card.Description>
+                        </Card>
+                    </div>
+
+                    <div>
+                        <Card ctaText="" href="/modules" hasBorder fullWidth={true} >
+                            <Card.Heading>Moodulid</Card.Heading>
+                            <Card.Description>
+                                Jaga kursuseid moodulite vahel. Kontrolli moodulite ning õppekava täituvust.
+                            </Card.Description>
+                        </Card>
+                    </div>
+                </div>
+
+            </div>
+        </BrandTheme>
+
     );
 }
 export default LandingPage;
